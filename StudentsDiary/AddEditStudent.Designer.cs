783b4d0faf85c97aeeb26b18c0a73a57bf706b3f
Robert Wolniewicz <robert_wol@wp.pl>
2023-04-18 20:07:50 +0200

    Add project files.

---
+++ StudentsDiary/AddEditStudent.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace StudentsDiary
+{
+    partial class AddEditStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbId = new System.Windows.Forms.Label();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.lbFirstName = new System.Windows.Forms.Label();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.lbLastName = new System.Windows.Forms.Label();
+            this.tbTechnology = new System.Windows.Forms.TextBox();
+            this.lbTechnology = new System.Windows.Forms.Label();
+            this.tbPhysics = new System.Windows.Forms.TextBox();
+            this.lbPhysics = new System.Windows.Forms.Label();
+            this.tbForeingLang = new System.Windows.Forms.TextBox();
+            this.lbMath = new System.Windows.Forms.Label();
+            this.tbPolishLang = new System.Windows.Forms.TextBox();
+            this.lbPolishLang = new System.Windows.Forms.Label();
+            this.tbMath = new System.Windows.Forms.TextBox();
+            this.lbForeingLang = new System.Windows.Forms.Label();
+            this.rtbComents = new System.Windows.Forms.RichTextBox();
+            this.lbComments = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbId
+            // 
+            this.lbId.AutoSize = true;
+            this.lbId.Location = new System.Drawing.Point(12, 12);
+            this.lbId.Name = "lbId";
+            this.lbId.Size = new System.Drawing.Size(17, 15);
+            this.lbId.TabIndex = 0;
+            this.lbId.Text = "Id";
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(110, 12);
+            this.tbId.Name = "tbId";
+            this.tbId.ReadOnly = true;
+            this.tbId.Size = new System.Drawing.Size(186, 23);
+            this.tbId.TabIndex = 1;
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(110, 41);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(186, 23);
+            this.tbFirstName.TabIndex = 3;
+            // 
+            // lbFirstName
+            // 
+            this.lbFirstName.AutoSize = true;
+            this.lbFirstName.Location = new System.Drawing.Point(12, 41);
+            this.lbFirstName.Name = "lbFirstName";
+            this.lbFirstName.Size = new System.Drawing.Size(30, 15);
+            this.lbFirstName.TabIndex = 2;
+            this.lbFirstName.Text = "Imie";
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(110, 70);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(186, 23);
+            this.tbLastName.TabIndex = 5;
+            // 
+            // lbLastName
+            // 
+            this.lbLastName.AutoSize = true;
+            this.lbLastName.Location = new System.Drawing.Point(12, 70);
+            this.lbLastName.Name = "lbLastName";
+            this.lbLastName.Size = new System.Drawing.Size(57, 15);
+            this.lbLastName.TabIndex = 4;
+            this.lbLastName.Text = "Nazwisko";
+            // 
+            // tbTechnology
+            // 
+            this.tbTechnology.Location = new System.Drawing.Point(110, 128);
+            this.tbTechnology.Name = "tbTechnology";
+            this.tbTechnology.Size = new System.Drawing.Size(186, 23);
+            this.tbTechnology.TabIndex = 7;
+            // 
+            // lbTechnology
+            // 
+            this.lbTechnology.AutoSize = true;
+            this.lbTechnology.Location = new System.Drawing.Point(12, 128);
+            this.lbTechnology.Name = "lbTechnology";
+            this.lbTechnology.Size = new System.Drawing.Size(71, 15);
+            this.lbTechnology.TabIndex = 6;
+            this.lbTechnology.Text = "Technologia";
+            // 
+            // tbPhysics
+            // 
+            this.tbPhysics.Location = new System.Drawing.Point(110, 157);
+            this.tbPhysics.Name = "tbPhysics";
+            this.tbPhysics.Size = new System.Drawing.Size(186, 23);
+            this.tbPhysics.TabIndex = 9;
+            this.tbPhysics.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // lbPhysics
+            // 
+            this.lbPhysics.AutoSize = true;
+            this.lbPhysics.Location = new System.Drawing.Point(12, 157);
+            this.lbPhysics.Name = "lbPhysics";
+            this.lbPhysics.Size = new System.Drawing.Size(39, 15);
+            this.lbPhysics.TabIndex = 8;
+            this.lbPhysics.Text = "Fizyka";
+            // 
+            // tbForeingLang
+            // 
+            this.tbForeingLang.Location = new System.Drawing.Point(110, 215);
+            this.tbForeingLang.Name = "tbForeingLang";
+            this.tbForeingLang.Size = new System.Drawing.Size(186, 23);
+            this.tbForeingLang.TabIndex = 11;
+            // 
+            // lbMath
+            // 
+            this.lbMath.AutoSize = true;
+            this.lbMath.Location = new System.Drawing.Point(12, 99);
+            this.lbMath.Name = "lbMath";
+            this.lbMath.Size = new System.Drawing.Size(73, 15);
+            this.lbMath.TabIndex = 10;
+            this.lbMath.Text = "Matematyka";
+            // 
+            // tbPolishLang
+            // 
+            this.tbPolishLang.Location = new System.Drawing.Point(110, 186);
+            this.tbPolishLang.Name = "tbPolishLang";
+            this.tbPolishLang.Size = new System.Drawing.Size(186, 23);
+            this.tbPolishLang.TabIndex = 13;
+            // 
+            // lbPolishLang
+            // 
+            this.lbPolishLang.AutoSize = true;
+            this.lbPolishLang.Location = new System.Drawing.Point(12, 186);
+            this.lbPolishLang.Name = "lbPolishLang";
+            this.lbPolishLang.Size = new System.Drawing.Size(57, 15);
+            this.lbPolishLang.TabIndex = 12;
+            this.lbPolishLang.Text = "Język Pol.";
+            // 
+            // tbMath
+            // 
+            this.tbMath.Location = new System.Drawing.Point(110, 99);
+            this.tbMath.Name = "tbMath";
+            this.tbMath.Size = new System.Drawing.Size(186, 23);
+            this.tbMath.TabIndex = 15;
+            // 
+            // lbForeingLang
+            // 
+            this.lbForeingLang.AutoSize = true;
+            this.lbForeingLang.Location = new System.Drawing.Point(12, 215);
+            this.lbForeingLang.Name = "lbForeingLang";
+            this.lbForeingLang.Size = new System.Drawing.Size(60, 15);
+            this.lbForeingLang.TabIndex = 14;
+            this.lbForeingLang.Text = "Język obc.";
+            // 
+            // rtbComents
+            // 
+            this.rtbComents.Location = new System.Drawing.Point(110, 244);
+            this.rtbComents.Name = "rtbComents";
+            this.rtbComents.Size = new System.Drawing.Size(186, 96);
+            this.rtbComents.TabIndex = 16;
+            this.rtbComents.Text = "";
+            // 
+            // lbComments
+            // 
+            this.lbComments.AutoSize = true;
+            this.lbComments.Location = new System.Drawing.Point(12, 244);
+            this.lbComments.Name = "lbComments";
+            this.lbComments.Size = new System.Drawing.Size(40, 15);
+            this.lbComments.TabIndex = 17;
+            this.lbComments.Text = "Uwagi";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(110, 346);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(82, 31);
+            this.btnCancel.TabIndex = 18;
+            this.btnCancel.Text = "Anuluj";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(208, 346);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(88, 31);
+            this.btnAdd.TabIndex = 19;
+            this.btnAdd.Text = "Zapisz";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // AddEditStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lbComments);
+            this.Controls.Add(this.rtbComents);
+            this.Controls.Add(this.tbMath);
+            this.Controls.Add(this.lbForeingLang);
+            this.Controls.Add(this.tbPolishLang);
+            this.Controls.Add(this.lbPolishLang);
+            this.Controls.Add(this.tbForeingLang);
+            this.Controls.Add(this.lbMath);
+            this.Controls.Add(this.tbPhysics);
+            this.Controls.Add(this.lbPhysics);
+            this.Controls.Add(this.tbTechnology);
+            this.Controls.Add(this.lbTechnology);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.lbLastName);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.lbFirstName);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.lbId);
+            this.Name = "AddEditStudent";
+            this.Text = "AddEditStudent";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lbId;
+        private TextBox tbId;
+        private TextBox tbFirstName;
+        private Label lbFirstName;
+        private TextBox tbLastName;
+        private Label lbLastName;
+        private TextBox tbTechnology;
+        private Label lbTechnology;
+        private TextBox tbPhysics;
+        private Label lbPhysics;
+        private TextBox tbForeingLang;
+        private Label lbMath;
+        private TextBox tbPolishLang;
+        private Label lbPolishLang;
+        private TextBox tbMath;
+        private Label lbForeingLang;
+        private RichTextBox rtbComents;
+        private Label lbComments;
+        private Button btnCancel;
+        private Button btnAdd;
+    }
+}
---
+++ StudentsDiary/Main.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace StudentsDiary
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnRefreshe = new System.Windows.Forms.Button();
+            this.dgvDiary = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDiary)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnAdd.Location = new System.Drawing.Point(12, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Dodaj";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.Orange;
+            this.btnEdit.Location = new System.Drawing.Point(93, 12);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edytuj";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(174, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Usuń";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnRefreshe
+            // 
+            this.btnRefreshe.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnRefreshe.Location = new System.Drawing.Point(255, 12);
+            this.btnRefreshe.Name = "btnRefreshe";
+            this.btnRefreshe.Size = new System.Drawing.Size(75, 23);
+            this.btnRefreshe.TabIndex = 3;
+            this.btnRefreshe.Text = "Odświerz";
+            this.btnRefreshe.UseVisualStyleBackColor = false;
+            this.btnRefreshe.Click += new System.EventHandler(this.btnRefreshe_Click);
+            // 
+            // dgvDiary
+            // 
+            this.dgvDiary.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDiary.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvDiary.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDiary.Location = new System.Drawing.Point(12, 41);
+            this.dgvDiary.Name = "dgvDiary";
+            this.dgvDiary.RowTemplate.Height = 25;
+            this.dgvDiary.Size = new System.Drawing.Size(776, 397);
+            this.dgvDiary.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvDiary);
+            this.Controls.Add(this.btnRefreshe);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDiary)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnAdd;
+        private Button btnEdit;
+        private Button btnDelete;
+        private Button btnRefreshe;
+        private DataGridView dgvDiary;
+    }
+}
